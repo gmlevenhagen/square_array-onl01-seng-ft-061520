a593123c3853c4110ccb3cd655fcb04d08bdb1ac
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-18 16:46:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  new_numbers = []
-    array.each { |numbers| new_numbers << ** 2}
-    new_numbers
-    end
+   new_numbers = []
+   array.each { |numbers| new_numbers << ** 2}
+   new_numbers
+ end
 end
